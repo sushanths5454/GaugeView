1a51f6b71e400440790b391e760b6c0b2a9626e0
SushanthSuvarna <sushanths5454@gmail.com>
2024-08-15 19:05:00 +0530

    Needle view with animation added

--- GauageView/GauageView/ContentView.swift
+++ GauageView/GauageView/ContentView.swift
@@ -12,33 +12,74 @@ struct ContentView: View {
     @State var segmentColor = [Color.green, Color.gray, Color.yellow, Color.blue]
     var start = 0.0
     var end = 0.5
-    
+    @State var angle = 90
     var body: some View {
         
-        ZStack {
-            Circle()
-                .trim(from: 0.499, to: 1)
+        ZStack(alignment: .center) {
+            ZStack {
+                Circle()
+                    .trim(from: 0.499, to: 1)
                 .stroke(lineWidth: 22)
+                ForEach(0..<segement.count, id: \.self) {
+                    index in
+                    Circle()
+                        .trim(from: CGFloat(0.499/CGFloat(segement.count)) * CGFloat(index), to: CGFloat(0.499/CGFloat(segement.count) * CGFloat(index + 1)))
+                        .stroke(segmentColor[index], lineWidth: 20)
+                        .rotationEffect(.degrees(180))
+                }
+            }.frame(height: 300)
                 // .rotationEffect(.degrees(180))
             
-            ForEach(0..<segement.count, id: \.self) {
-                index in
-               // self.start = self.end
-                //end = 0.5/(segement.count - index)
-                Circle()
-                    .trim(from: CGFloat(0.499/CGFloat(segement.count)) * CGFloat(index), to: CGFloat(0.499/CGFloat(segement.count) * CGFloat(index + 1)))
-                    .stroke(segmentColor[index], lineWidth: 20)
-                    .rotationEffect(.degrees(180))
-                    
+            
+            ZStack() {
+                VStack(spacing: -10) {
+                    Triangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 100)
+                                .padding(.top, 40)
+                                .rotationEffect(.degrees(Double(angle)), anchor: .bottom)
+                            .animation(.easeInOut(duration: 1), value: angle)
+                    ZStack(alignment: .center) {
+                        Circle()
+                            .foregroundStyle(Color.black)
+                           // .padding(.vertical, 58.0)
+                            .frame(width: 20)
+                        Circle()
+                            .foregroundStyle(Color.white)
+                            .frame(width: 4)
+                    }//.frame(width: 20, height: 30)
+                   // .padding(.bottom, -50)
+                        
+                    Spacer()
+                }
                 
-            }
-           
+//                VStack {
+//                    Spacer()
+//                    
+//                    
+//                }.frame(width: 20, height: 100)
+                
+             
+               
+                Spacer()
+            }.frame(height: 300)
+
+            //Spacer()
+                //.rotationEffect(.degrees(90))
            //Circle()
                 
             
             
         }
+        .frame(height: 300)
         .padding()
+        .onAppear(perform: {
+            withAnimation(.easeOut, {
+                angle = -20
+            })
+            //anima
+           // angle = -90
+        })
     }
     
     struct Triangle: Shape {
@@ -55,6 +96,8 @@ struct ContentView: View {
 }
 
 #Preview {
-    NeedleView()
+    ContentView()
 }
+
+
 
